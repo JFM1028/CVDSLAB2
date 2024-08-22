b1a41bea974b71a281c8d53a420b6c20e0e8ed44
JFM1028 <116533238+JFM1028@users.noreply.github.com>
2024-08-22 16:07:01 -0500

    Add files via upload

---
+++ LAB2/Patterns/src/main/java/edu/eci/cvds/patterns/archetype/App.java
@@ -0,0 +1,16 @@
+package edu.eci.cvds.patterns.archetype;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Hello " + String.join(" ", args) + "!");
+        } else {
+            System.out.println("Hello World!");
+        }
+    }
+}
