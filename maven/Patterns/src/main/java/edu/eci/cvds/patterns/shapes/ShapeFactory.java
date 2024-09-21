13ba55089a6ef037691599931d652a20f3151256
Diego <58009401+marzo245@users.noreply.github.com>
2024-09-20 21:24:59 -0500

    arreglos para sustentacion

--- maven/Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
+++ maven/Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
@@ -1,31 +1,31 @@
 package edu.eci.cvds.patterns.shapes;
 
-import edu.eci.cvds.patterns.shapes.concrete.Triangle;
-import edu.eci.cvds.patterns.shapes.concrete.Quadrilateral;
-import edu.eci.cvds.patterns.shapes.concrete.Pentagon;
 import edu.eci.cvds.patterns.shapes.concrete.Hexagon;
+import edu.eci.cvds.patterns.shapes.concrete.Pentagon;
+import edu.eci.cvds.patterns.shapes.concrete.Quadrilateral;
+import edu.eci.cvds.patterns.shapes.concrete.Triangle;
 
 public class ShapeFactory {
-    
-    /**
-     * Crea una instancia de Shape basada en el tipo de forma proporcionado.
-     *
-     * @param type El tipo de forma que se desea crear.
-     * @return Una instancia de Shape correspondiente al tipo proporcionado.
-     * @throws IllegalArgumentException Si el tipo de forma no es reconocido.
-     */
-    public static Shape create(RegularShapeType type) {
-        switch (type) {
-            case TRIANGLE:
-                return new Triangle();
-            case QUADRILATERAL:
-                return new Quadrilateral();
-            case PENTAGON:
-                return new Pentagon();
-            case HEXAGON:
-                return new Hexagon();
-            default:
-                throw new IllegalArgumentException("Unknown shape type: " + type);
-        }
+
+  /**
+   * Crea una instancia de Shape basada en el tipo de forma proporcionado.
+   *
+   * @param type El tipo de forma que se desea crear.
+   * @return Una instancia de Shape correspondiente al tipo proporcionado.
+   * @throws IllegalArgumentException Si el tipo de forma no es reconocido.
+   */
+  public static Shape create(RegularShapeType type) {
+    switch (type) {
+      case Triangle:
+        return new Triangle();
+      case Quadrilateral:
+        return new Quadrilateral();
+      case Pentagon:
+        return new Pentagon();
+      case Hexagon:
+        return new Hexagon();
+      default:
+        throw new IllegalArgumentException("Unknown shape type: " + type);
     }
+  }
 }
